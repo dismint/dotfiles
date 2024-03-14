a432afc2b4134b4808703cae0699e5cbf667cf02
dismint <dismintjjc@gmail.com>
2024-03-14 12:59:49 -0400

    added new function to paste images from clipboard into a file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,23 @@ function fish_greeting
   # do nothing
 end
 
+function imgc
+    if test (count $argv) -lt 1
+        echo "USAGE: imgc <filename>"
+        return 1
+    end
+
+    set filename $argv[1]
+    wl-paste --type image/png | convert - $filename.png
+    echo "RESULT: image saved as $filename".png
+end
+
+
 # init / setup
 
 zoxide init fish | source
 
-# 
+# aliases
 
 alias cd="z"
 alias ls="eza"
