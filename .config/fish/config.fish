c154287757a7519eda4859b7971ae8b88b34f064
dismint <mintjjc@gmail.com>
2024-09-14 13:45:09 -0400

    add y script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,15 @@
 function fish_greeting
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 function starship_transient_prompt_func
   starship module character
 end
