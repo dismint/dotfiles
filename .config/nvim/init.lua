07f9be68a06ad0c001e946fcc855e1c924ebdfba
dismint <mintjjc@gmail.com>
2025-08-21 23:42:01 -0400

    remove smooth scroll

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -558,9 +558,6 @@ require("lazy").setup({
 		lazy = false,
 		---@type snacks.Config
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
 			bigfile = { enabled = true },
 			dashboard = { enabled = true },
 			explorer = { enabled = true },
@@ -570,7 +567,6 @@ require("lazy").setup({
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
 			scope = { enabled = true },
-			scroll = { enabled = true },
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
 		},
