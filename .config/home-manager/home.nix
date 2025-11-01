2b8fd0f915dda69548d31616a4c7daea09dc5136
dismint <mintjjc@gmail.com>
2025-11-01 09:02:59 -0400

    add home manager

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "dismint";
+  home.homeDirectory = "/home/dismint";
+
+  home.stateVersion = "25.05";
+
+  home.packages = [
+    # utilities
+    pkgs.gh
+    pkgs.git
+    pkgs.neovim
+    pkgs.ripgrep
+    pkgs.yazi
+    pkgs.starship
+    pkgs.eza
+    pkgs.fish
+    pkgs.zoxide
+    pkgs.stow
+    pkgs.unzip
+    pkgs.fzf
+    pkgs.openssh
+
+    # coding
+    pkgs.nodejs_24
+    pkgs.python314
+    pkgs.zig
+  ];
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  # ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/dismint/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = { EDITOR = "nvim"; };
+
+  programs.home-manager.enable = true;
+}
