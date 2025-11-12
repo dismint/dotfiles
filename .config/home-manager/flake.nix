4895444fd7fd07540ae5002fdc8afbe8da51a2cd
dismint <mintjjc@gmail.com>
2025-11-11 23:27:02 -0500

    changes

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of dismint";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
