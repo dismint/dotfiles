9ac16ea321e9bf6cf18eafe6233845a5043339a5
dismint <mintjjc@gmail.com>
2026-07-29 22:38:04 -0400

    add markdown

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -747,6 +747,13 @@ require("lazy").setup({
 		name = "zk",
 		opts = {},
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 	{ "kevinhwang91/nvim-bqf", ft = "qf" },
 	{ "dstein64/nvim-scrollview", opts = {} },
 	{ "brenoprata10/nvim-highlight-colors", opts = {} },
